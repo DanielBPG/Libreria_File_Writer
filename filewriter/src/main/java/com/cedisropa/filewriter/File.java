d93e4ec5fffc505619ad90dcfe9749507f62591a
Merielem Susana Perez Morales <merielem.perez@coppel.com>
2019-08-30 16:15:07 -0600

    Se suben cambios 2

--- filewriter/src/main/java/com/cedisropa/filewriter/File.java
+++ filewriter/src/main/java/com/cedisropa/filewriter/File.java
@@ -19,22 +19,22 @@ public class File {
      }
 
      public void write() throws IOException {
-         java.io.File root = new java.io.File(Environment.getExternalStorageDirectory(), "Lib");
+         java.io.File root = new java.io.File(Environment.getExternalStorageDirectory(), "Cambio");
          if (!root.exists()) {
              root.mkdirs();
          }
-         java.io.File gpxfile = new java.io.File(root, "PRUEBA_GUIAGENERADA.DAT");
+         java.io.File gpxfile = new java.io.File(root, "GUIAGENERADA.DAT");
          FileWriter writer = new FileWriter(gpxfile);
          writer.append(guia.getFecha());
          writer.append(guia.getCiudad());
          writer.append(guia.getNumGuia());
          writer.append(guia.getNomGuia());
-         writer.append(String.format("%1$04d",guia.getNumLote()));
+         writer.append(String.format("%1$05d",guia.getNumLote()));
          writer.append(guia.getTipoEmpaque());
          writer.append(String.valueOf(guia.getNumSurtidor()));
-         writer.append(String.format("%1$04d",guia.getUnidades()));
+         writer.append(String.format("%1$05d",guia.getUnidades()));
          writer.flush();
          writer.close();
-         Toast.makeText(context, "Archivo creado", Toast.LENGTH_SHORT).show();
+         Toast.makeText(context, "Se generó archivo", Toast.LENGTH_SHORT).show();
      }
 }
